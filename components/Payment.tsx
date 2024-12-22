f7f4d721ea91655556747d3712d065ee9503a6bf
Kanan Farzaliyev <kanan.farzali@gmail.com>
2024-12-22 13:51:26 +0100

    setting up stripe

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,8 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomButton from "./CustomButton";
+import { useStripe } from "@stripe/stripe-react-native";
+import { Alert } from "react-native";
+import { useLocationStore } from "@/store";
+import { PaymentProps } from "@/types/type";
 
-const Payment = () => {
-  const openPaymentSheet = async () => {};
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const {
+    userAddress,
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationAddress,
+    destinationLongitude,
+  } = useLocationStore();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: parseInt(amount) * 100,
+          currencyCode: "usd",
+        },
+        confirmHandler: async (
+          paymentMethod,
+          shouldSavePaymentMethod,
+          intentCreationCallback
+        ) => {
+          //   const { paymentIntent, customer } = await fetchAPI(
+          //     "/(api)/(stripe)/create",
+          //     {
+          //       method: "POST",
+          //       headers: {
+          //         "Content-Type": "application/json",
+          //       },
+          //       body: JSON.stringify({
+          //         name: fullName || email.split("@")[0],
+          //         email: email,
+          //         amount: amount,
+          //         paymentMethodId: paymentMethod.id,
+          //       }),
+          //     }
+          //   );
+          //   if (paymentIntent.client_secret) {
+          //     const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+          //       method: "POST",
+          //       headers: {
+          //         "Content-Type": "application/json",
+          //       },
+          //       body: JSON.stringify({
+          //         payment_method_id: paymentMethod.id,
+          //         payment_intent_id: paymentIntent.id,
+          //         customer_id: customer,
+          //         client_secret: paymentIntent.client_secret,
+          //       }),
+          //     });
+          //     if (result.client_secret) {
+          //       await fetchAPI("/(api)/ride/create", {
+          //         method: "POST",
+          //         headers: {
+          //           "Content-Type": "application/json",
+          //         },
+          //         body: JSON.stringify({
+          //           origin_address: userAddress,
+          //           destination_address: destinationAddress,
+          //           origin_latitude: userLatitude,
+          //           origin_longitude: userLongitude,
+          //           destination_latitude: destinationLatitude,
+          //           destination_longitude: destinationLongitude,
+          //           ride_time: rideTime.toFixed(0),
+          //           fare_price: parseInt(amount) * 100,
+          //           payment_status: "paid",
+          //           driver_id: driverId,
+          //           user_id: userId,
+          //         }),
+          //       });
+          //       intentCreationCallback({
+          //         clientSecret: result.client_secret,
+          //       });
+          //     }
+          //   }
+        },
+      },
+      returnURL: "myapp://book-ride",
+    });
+
+    if (!error) {
+      // setLoading(true);
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
 
   return (
     <>
